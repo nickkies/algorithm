553b3326dcd976227c7006533cffc4dbd50571f5
YoonSung Kim <opqudk@gmail.com>
2023-12-27 11:48:51 +0900

    ⚗️ fibonacci dynamic

--- theory/src/dynamic_programing.rs
+++ theory/src/dynamic_programing.rs
@@ -1,26 +1,37 @@
 #[cfg(test)]
 mod tests {
-    fn recursive(n: usize) -> usize {
+    fn fibo_recursive(n: usize) -> usize {
         if n <= 1 {
             n
         } else {
-            recursive(n - 1) + recursive(n - 2)
+            fibo_recursive(n - 1) + fibo_recursive(n - 2)
         }
     }
 
-    fn dynamic(n: usize) -> usize {
+    fn fibo_dynamic(n: usize, memo: &mut Vec<Option<usize>>) -> usize {
         if n == 0 {
             return 0;
         } else if n == 1 {
             return 1;
-        } else {
-            return dynamic(n - 1) + dynamic(n - 2);
         }
+
+        if let Some(val) = memo[n] {
+            return val;
+        }
+
+        let res = fibo_dynamic(n - 1, memo) + fibo_dynamic(n - 2, memo);
+        memo[n] = Some(res);
+        res
     }
 
     #[test]
     fn test_recursive() {
-        assert_eq!(recursive(10), 55);
-        assert_eq!(dynamic(10), 55);
+        let n = 10;
+        let a = 55;
+
+        assert_eq!(fibo_recursive(n), a);
+
+        let mut memo = vec![None; n + 1];
+        assert_eq!(fibo_dynamic(n, &mut memo), a);
     }
 }
