d8078f165212869050cb47a3bfa2eb5c0e4e4ad4
YoonSung Kim <opqudk@gmail.com>
2023-12-29 15:35:57 +0900

    ⚗️ tile a 2 x n grid using 2 x 1 tiles

--- theory/src/dynamic_programing.rs
+++ theory/src/dynamic_programing.rs
@@ -33,4 +33,25 @@ mod tests {
         let mut memo = vec![None; n + 1];
         assert_eq!(fibo_dynamic(n, &mut memo), a);
     }
+
+    /// ### 문제
+    ///
+    /// 2 * n 크기의 직사각형을 1 * 2, 2 * 1 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
+    ///
+    /// ### 입력
+    ///
+    /// 첫째 줄에 n이 주어진다 `1 <= n <= 1,000`
+    #[test]
+    fn test_n_times_2_tiling() {
+        let n: usize = 9;
+        let mut memo = vec![0; n + 1];
+        memo[1] = 1;
+        memo[2] = 2;
+
+        for i in 3..=n {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+
+        assert_eq!(memo[n], 55);
+    }
 }
