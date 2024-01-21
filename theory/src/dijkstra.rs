2e0a5c01e4f29706cd02016d492fece0c8771f51
YoonSung Kim <opqudk@gmail.com>
2024-01-21 17:35:04 +0900

    ⚗️ dijkstra

--- theory/src/dijkstra.rs
+++ theory/src/dijkstra.rs
@@ -1,10 +1,79 @@
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
+    use log::debug;
+    use std::{
+        cmp::Ordering,
+        collections::{BinaryHeap, HashMap, HashSet},
+    };
+
+    #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+    struct Node {
+        name: char,
+        distance: usize,
+    }
+
+    impl Ord for Node {
+        fn cmp(&self, other: &Self) -> Ordering {
+            other.distance.cmp(&self.distance)
+        }
+    }
+
+    impl PartialOrd for Node {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
 
     type Graph = HashMap<char, HashMap<char, usize>>;
 
-    fn dijkstra(graph: Graph, (start, end): (char, char)) -> Option<usize> {
+    fn dijkstra(mut graph: Graph, (start, end): (char, char)) -> Option<usize> {
+        let mut distances: HashMap<char, usize> = HashMap::new();
+        let mut visited: HashSet<char> = HashSet::new();
+        let mut priority_queue: BinaryHeap<Node> = BinaryHeap::new();
+
+        distances.insert(start, 0);
+        priority_queue.push(Node {
+            name: start,
+            distance: 0,
+        });
+
+        while let Some(Node { name, distance }) = priority_queue.pop() {
+            if name == end {
+                debug!("graph: {graph:?}");
+                debug!("distances: {distances:?}");
+                debug!("visited: {visited:?}");
+                debug!("priority_queue: {priority_queue:?}");
+                return Some(distance);
+            }
+
+            if visited.contains(&name) {
+                continue;
+            }
+
+            visited.insert(name);
+
+            if let Some(neighbors) = graph.remove(&name) {
+                for (neighbor, weight) in neighbors {
+                    let new_distance = distance + weight;
+                    if let Some(&current_distance) = distances.get(&neighbor) {
+                        if new_distance < current_distance {
+                            distances.insert(neighbor, new_distance);
+                            priority_queue.push(Node {
+                                name: neighbor,
+                                distance: new_distance,
+                            });
+                        }
+                    } else {
+                        distances.insert(neighbor, new_distance);
+                        priority_queue.push(Node {
+                            name: neighbor,
+                            distance: new_distance,
+                        });
+                    }
+                }
+            }
+        }
+
         None
     }
 
